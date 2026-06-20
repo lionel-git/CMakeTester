9f181bf4786faeb57c3278cf0e8ca20104507ced
Lionel Désorme <lionel.desorme@free.fr>
2026-06-20 10:28:02 +0200

    Initial commit

---
+++ CMakeTester.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeTester.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeTester.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
